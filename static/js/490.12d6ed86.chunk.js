16638722f3fc2d7f5fdec22305f4628a09c56e66
M-Michaell <michaelmaherhalim@gmail.com>
2023-10-08 13:42:21 +0300

    build

---
+++ static/js/490.12d6ed86.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirst=self.webpackChunkfirst||[]).push([[490],{3490:function(n,r,t){t.r(r),t.d(r,{default:function(){return e}});t(2791);var u=t(184);function e(){return(0,u.jsx)("div",{children:"Login"})}}}]);
+//# sourceMappingURL=490.12d6ed86.chunk.js.map
